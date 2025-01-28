cb7d6b3bc0484a664938df4b62af07e440929d7e
H4XO-999 <h4ck3r.999.999.999.999@gmail.com>
2025-01-28 01:54:22 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Hamburger Menu Toggle
+document.getElementById('menu-toggle').addEventListener('click', () => {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('hidden');
+});
